ee79af66c8c2c7602cab45cc59a1c85459e9394e
lucifer <lucifer>
2019-10-25 15:33:02 +0800

    集成dubbo

---
+++ dubbo/README.md
@@ -0,0 +1,2 @@
+## **`项目简介`**
+`dubbo` 是 `spring boot + dubbo` 基于`zookeeper`快速搭建一个demo
---
+++ dubbo/dubbo-api/src/main/java/com/sweet/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sweet.service;
+
+import java.util.List;
+
+import com.sweet.model.User;
+
+public interface UserService {
+
+    List<User> listUser();
+}
