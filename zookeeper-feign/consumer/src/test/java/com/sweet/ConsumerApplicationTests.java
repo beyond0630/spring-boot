5a0f3c91a84724b99534d434297f92f29da507e8
lucifer <lucifer>
2019-11-04 11:20:10 +0800

    feign mvn package 报错修复

--- zookeeper-feign/consumer/src/test/java/com/sweet/ConsumerApplicationTests.java
+++ zookeeper-feign/consumer/src/test/java/com/sweet/ConsumerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConsumerApplicationTests {
+public class ConsumerApplicationTests {
 
     @Test
     void contextLoads() {
