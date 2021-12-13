528c4fd7a769e8ebf3a552ac13b27ea09ac12b45
Beyond <bin_beyond@outlook.com>
2021-12-13 09:50:08 +0800

    fix: 修改 nacos-config 名称

--- nacos-config/src/main/java/org/beyond/nacos/config/NacosConfigApplication.java
+++ nacos-config/src/main/java/org/beyond/nacos/config/NacosConfigApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Beyond
  */
 @SpringBootApplication
-public class BeyondNacosConfigApplication {
+public class NacosConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeyondNacosConfigApplication.class, args);
+        SpringApplication.run(NacosConfigApplication.class, args);
     }
 }
